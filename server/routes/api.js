8e0a6bbac5be1bb9b9b0497fce4a0a452f7b0e06
rafeq abu fiad <rafeqabufiad@rafeqs-MacBook-Pro.local>
2023-03-20 14:25:28 +0200

    add routers

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,4 +11,34 @@ router.get("/Todo", function (req, res) {
   });
 });
 
+router.post("/todoCreate", (req, res) => {
+  const Todo = new MonogDB({
+      name: req.body.name,
+      date: req.body.date,
+      description:req.body.description,
+      isDone:req.body.isDone
+  })
+  Todo.save().then((res) => {
+      console.log(res+"added to db")
+  })
+  res.end()
+})
+
+
+router.delete('/deleteTodo/:todoName', function (req, res) {
+  let todoName = req.params.todoName
+  MonogDB.deleteOne({name:todoName}).then((todo)=>{
+      console.log(todo+"daleted");
+  })
+  res.end()
+})
+
+router.put('/editDescription/:todoName', function (req, res) {
+  let todoName = req.params.todoName
+  MonogDB.find({name:todoName}).then((todo)=>{
+      todo.description=todo
+  })
+  res.end()
+})
+
 module.exports = router;
