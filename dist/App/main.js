de7b8af4ac0ceb68c61aff857d4310b8f7d33778
or hadad <orhadad19@gmail.com>
2023-03-21 19:54:06 +0200

    remove manager render

--- dist/App/main.js
+++ dist/App/main.js
@@ -1,19 +1,19 @@
 const render = new Render();
 // זה הדאטא שנכנס 
-let mokdata = {
-  name: "to do",
-  isDone: true,
-  date: "2023-03-23",
-  description: "hi dear my name",
-};
-let mok = {
-  name: "go to work",
-  isDone: false,
-  date: "2023-03-25",
-  description: "i need to dfo",
-};
-render.addEvent(mokdata); // render new event to page calender
-render.addEvent(mok);
+// let mokdata = {
+//   name: "to do",
+//   isDone: true,
+//   date: "2023-03-23",
+//   description: "hi dear my name",
+// };
+// let mok = {
+//   name: "go to work",
+//   isDone: true,
+//   date: "2023-03-25",
+//   description: "i need to dfo",
+// };
+// renderManager.renderCalendar.addEvent(mokdata); // render new event to page calender
+// render.addEvent(mok);
 
 $("body").on("click", ".delete-btn", function () {
   const footer = $(this).parent();
