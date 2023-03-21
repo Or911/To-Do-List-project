06938c02f570c2efe6905a4a455194d21a7f0577
or hadad <orhadad19@gmail.com>
2023-03-21 10:56:53 +0200

    add calender

--- dist/render.js
+++ dist/render.js
@@ -1,36 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      events: [
-        {
-        //   title: 'לעשות כושר ',
-          start: '2023-03-20',
-          image_url: 'https://cdn-icons-png.flaticon.com/512/4436/4436481.png',
-        },
-      ],
-      eventContent: function(arg) {
-        let arrayOfDomNodes = []
-        // title event
-        let titleEvent = document.createElement('div')
-        if(arg.event._def.title) {
-          titleEvent.innerHTML = arg.event._def.title
-          titleEvent.classList = "fc-event-title fc-sticky"
+class Render {
+  constructor() {
+    this.events = [];
+  }
+
+  addEvent(todoList) {
+    let newEvent = this.sortDataToEvent(todoList);
+    this.events.push(newEvent);
+    this.calendar();
+  }
+  sortDataToEvent(todoList) {
+    const imgDone = "https://cdn-icons-png.flaticon.com/512/4436/4436481.png";
+    let newEvent = {
+      title: todoList.name,
+      start: new Date(),
+      end: todoList.date,
+    };
+    if (todoList.isDone) {
+      newEvent.image_url = imgDone;
+    }
+    return newEvent;
+  }
+
+  calendar() {
+    const calendarHtml = document.getElementById("calendar-container");
+    const calendar = new FullCalendar.Calendar(calendarHtml, {
+      initialView: "dayGridMonth",
+      events: this.events,
+      eventContent: function (arg) {
+        let arrayOfDomNodes = [];
+
+        let titleEvent = document.createElement("div");
+        if (arg.event._def.title) {
+          titleEvent.innerHTML = arg.event._def.title;
+          titleEvent.classList = "fc-event-title fc-sticky";
         }
-  
-        // image event
-        let imgEventWrap = document.createElement('div')
-        if(arg.event.extendedProps.image_url) {
-          let imgEvent = '<img src="'+arg.event.extendedProps.image_url+'" >'
-          imgEventWrap.classList = "fc-event-img"
+
+        let imgEventWrap = document.createElement("div");
+        if (arg.event.extendedProps.image_url) {
+          let imgEvent =
+            '<img src="' + arg.event.extendedProps.image_url + '" >';
+          imgEventWrap.classList = "fc-event-img";
           imgEventWrap.innerHTML = imgEvent;
         }
-  
-        arrayOfDomNodes = [ titleEvent,imgEventWrap ]
-  
-        return { domNodes: arrayOfDomNodes }
+
+        arrayOfDomNodes = [titleEvent, imgEventWrap];
+
+        return { domNodes: arrayOfDomNodes };
       },
     });
     calendar.render();
-  
-  });
+  }
+}
