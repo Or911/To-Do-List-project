407398806e8436c7e136da22ca29db5bc8cb1f7c
Almog <almo321@gmail.com>
2023-03-21 09:56:23 +0200

    add giphy gif

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,39 +1,48 @@
 const express = require("express");
 const MonogDB = require("../model/DBschema");
-const dataManager=require("../utilities/dataManager");
+const dataManager = require("../utilities/dataManager");
 
 const router = express.Router();
 
 router.get("/Todo", function (req, res) {
   MonogDB.find({})
-  .sort({ date: -1 })
-  .then(function (todoCards) {
-    res.send(todoCards);
-  });
+    .sort({ date: -1 })
+    .then(function (todoCards) {
+      res.send(todoCards);
+    });
 });
 
-router.post("/todoCreate", (req, res) => {
-   
-   data.addTodoList(req)
-   
-  res.end()
-})
+router.get("/Todo/:name", function (req, res) {
+  MonogDB.findOne({name : req.params.name})
+    .then(function (todoCard) {
+      res.send(todoCard);
+    });
+});
 
+router.post("/Todo", (req, res) => {
 
-router.delete('/deleteTodo/:todoName', function (req, res) {
-  let todoName = req.params.todoName
-  MonogDB.deleteOne({name:todoName}).then((todo)=>{
-      console.log(todo+"daleted");
-  })
-  res.end()
-})
+  dataManager.addTodoCard(req)
 
-router.put('/editDescription/:todoName', function (req, res) {
-  let todoName = req.params.todoName
-  MonogDB.find({name:todoName}).then((todo)=>{
-      todo.description=todo
-  })
-  res.end()
-})
+  res.end();
+});
+
+router.delete("/Todo/:name", function (req, res) {
+  let todoName = req.params.name;
+  MonogDB.deleteOne({ name: todoName }).then((todo) => {
+    console.log(todo + "daleted");
+  });
+  res.end();
+});
+
+router.put("/Todo/:name", function (req, res) {
+  let todoName = req.params.name;
+  let description = req.body.description;
+
+  MonogDB.findOneAndUpdate({ name: todoName } , {description : description} ).then((todo) => {
+    console.log(todo);
+  });
+
+  res.end();
+});
 
 module.exports = router;
